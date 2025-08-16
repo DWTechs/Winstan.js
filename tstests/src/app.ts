1761f4db1c8496dcd45b33c0528ccb13f3e53e27
LCluber <ludovic.cluber@gmail.com>
2025-08-16 09:54:29 +0200

    docs: streamline init method call formatting in app.ts for consistency

--- tstests/src/app.ts
+++ tstests/src/app.ts
@@ -1,26 +1,12 @@
 import { init, log } from "../../dist/winstan.js";
 
-init(
-  "europe/paris",
-  "fr-FR",
-  "ms-user",
-  "debug"
-);
-
+init("europe/paris","fr-FR","ms-user","debug");
 log.error("App did not start");
-
 log.warn("App did not start properly");
-
 log.info("App started on port : 3000");
-
 log.debug("UpdateOne(user=4)");
-
 log.debug("UpdateOne(user=4)", { requestId:4554645 });
-
 log.debug("UpdateOne(user=4)", { requestId:4554645, userId: 65.6 });
-
 log.debug("UpdateOne(user=4)", { requestId:4554645, userId: 45 });
-
 log.debug("UpdateOne(user=4)", { requestId:4554645, userId: 45, tags: [ "update" ] });
-
 log.debug("UpdateOne(user=4)", { requestId:4554645, userId: 45, tags:  [ "update", "user" ] });
